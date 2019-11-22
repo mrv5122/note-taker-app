ebb60294ad1044a45ae5608020157fd27c8858bd
Manuel R <mannyvazqsan1@icloud.com>
2019-11-21 19:18:34 -0500

    fixed htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,15 +3,14 @@ var router = require('express').Router();
 
 // https://expressjs.com/en/guide/routing.html
 
-// "/notes" responds with the notes.html file
-// TODO: Define a route for your `notes.html` file
-//    and serve it from `public/`
-// https://expressjs.com/en/4x/api.html#app.get.method
-// https://expressjs.com/en/api.html#res.sendFile
-
 // All other routes respond with the index.html file
 router.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// "/notes" responds with the notes.html file
+router.get('/notes', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 module.exports = router;
